f158b77445fb6758c623ae3287cdb14fdf69b10f
Mehr Licht! <mehrlicht@gmail.com>
2019-02-11 18:46:15 +0000

    added checkDigit

---
+++ checkDigit/checkdigit/Form1.Designer.vb
@@ -0,0 +1,99 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.numerotxtbx = New System.Windows.Forms.TextBox()
+        Me.aviadotxtbx = New System.Windows.Forms.TextBox()
+        Me.verifbtn = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'numerotxtbx
+        '
+        Me.numerotxtbx.Font = New System.Drawing.Font("Calibri", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.numerotxtbx.Location = New System.Drawing.Point(56, 42)
+        Me.numerotxtbx.Name = "numerotxtbx"
+        Me.numerotxtbx.Size = New System.Drawing.Size(168, 33)
+        Me.numerotxtbx.TabIndex = 0
+        '
+        'aviadotxtbx
+        '
+        Me.aviadotxtbx.Font = New System.Drawing.Font("Calibri", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.aviadotxtbx.Location = New System.Drawing.Point(56, 120)
+        Me.aviadotxtbx.MaxLength = 7
+        Me.aviadotxtbx.Name = "aviadotxtbx"
+        Me.aviadotxtbx.Size = New System.Drawing.Size(168, 33)
+        Me.aviadotxtbx.TabIndex = 1
+        '
+        'verifbtn
+        '
+        Me.verifbtn.Font = New System.Drawing.Font("Calibri", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.verifbtn.Location = New System.Drawing.Point(100, 191)
+        Me.verifbtn.Name = "verifbtn"
+        Me.verifbtn.Size = New System.Drawing.Size(86, 32)
+        Me.verifbtn.TabIndex = 2
+        Me.verifbtn.Text = "verificar"
+        Me.verifbtn.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(56, 26)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(141, 13)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "código a verificar (####.#$)"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(56, 104)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(72, 13)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "nº de aviados"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.verifbtn)
+        Me.Controls.Add(Me.aviadotxtbx)
+        Me.Controls.Add(Me.numerotxtbx)
+        Me.Name = "Form1"
+        Me.Text = "check digit"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents numerotxtbx As System.Windows.Forms.TextBox
+    Friend WithEvents aviadotxtbx As System.Windows.Forms.TextBox
+    Friend WithEvents verifbtn As System.Windows.Forms.Button
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+
+End Class
---
+++ checkDigit/checkdigit/Form1.vb
@@ -0,0 +1,178 @@
+﻿Imports System.Text
+
+Public Class Form1
+
+
+    Dim s As String
+    Dim n As Double
+    Dim r As Double
+    Dim caractp1 As String
+    Dim valorponto As String
+    Dim valor As String
+    Dim numero As String
+    Dim check As String
+    Dim aviados As String
+    Dim number As String
+    Dim foco As String
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Me.KeyPreview = True
+        limpar()
+    End Sub
+
+    Private Sub frmDesigner_KeyPress(ByVal sender As Object, ByVal e As System.Windows.Forms.KeyPressEventArgs) Handles Me.KeyPress
+1:      foco = Me.ActiveControl.Name()
+    End Sub
+
+
+
+
+
+    Private Sub data_KeysEnter(ByVal sender As Object, ByVal e As System.Windows.Forms.KeyEventArgs) Handles Me.KeyUp
+
+        If e.KeyCode = Keys.Enter Then
+
+            Select Case foco
+                Case Is = "numerotxtbx"
+41:                 If numerotxtbx.Text <> "" Then
+                        Me.aviadotxtbx.Focus()
+                    End If
+42:             Case Is = "aviadotxtbx"
+                    If aviadotxtbx.Text <> "" Then
+                        Me.verifbtn.Focus()
+                        Me.verifbtn.Select()
+                    Else
+                        Me.aviadotxtbx.Focus()
+                    End If
+                Case Is = "verifbtn"
+                    clickar()
+                Case Is = "form1"
+                    limpar()
+                Case Else
+                    numerotxtbx.Focus()
+            End Select
+        End If
+    End Sub
+
+    Sub clickar()
+        On Error GoTo MOSTRARERRO
+
+        If numerotxtbx.Text = "" Or aviadotxtbx.Text = "" Then
+            limpar()
+            Exit Sub
+        End If
+2:      number = numerotxtbx.Text
+3:      aviados = aviadotxtbx.Text
+4:      check = number.Substring(Len(number) - 1, 1).ToUpper
+5:      'ordem = number.Substring(0, Len(number) - 1)
+6:      numero = getNumeric(number.Substring(0, Len(number) - 2))
+      
+7:      s = aviados & numero
+8:
+9:      n = Val(s)
+10:     While n > 26
+11:         s = 0
+12:         While n > 0
+13:             r = n Mod 10
+14:             s = s + r
+15:             n = n \ 10
+16:         End While
+17:         n = s
+18:     End While
+19:     valor = numero & aviados & letrar(s)
+20:     valorponto = numero & "." & aviados & letrar(s)
+       
+21:     If valor = UCase(numerotxtbx.Text) Or valorponto = UCase(numerotxtbx.Text) Then
+22:         If MessageBox.Show("OK", "OK", MessageBoxButtons.OKCancel) Then
+
+23:             limpar()
+                numerotxtbx.Focus()
+24:         End If
+25:     Else
+26:         If MessageBox.Show("Não Bate Certo", "ERRO!", MessageBoxButtons.OKCancel) Then
+
+27:             limpar()
+                numerotxtbx.Focus()
+28:         End If
+29:     End If
+30:     Exit Sub
+MOSTRARERRO:
+        MsgBox("Sub Clickar: Erro número #" & Str$(Err.Number) & " na linha " & Str$(Erl) & " - " & Err.Description & " - gerado por " & Err.Source)
+        Resume Next
+    End Sub
+
+
+
+    Sub limpar()
+1:      On Error GoTo MOSTRARERRO
+        caractp1 = ""
+2:      valor = ""
+3:      numero = ""
+4:      check = ""
+5:      aviados = ""
+6:      number = ""
+7:      numerotxtbx.Text = ""
+8:      aviadotxtbx.Text = ""
+        Me.numerotxtbx.Focus()
+
+9:      Exit Sub
+MOSTRARERRO:
+        MsgBox("Sub limpar: Erro número #" & Str$(Err.Number) & " na linha " & Str$(Erl) & " - " & Err.Description & " - gerado por " & Err.Source)
+        Resume Next
+    End Sub
+
+
+
+    Private Sub verifbtn_Click(sender As Object, e As EventArgs) Handles verifbtn.Click
+1:      On Error GoTo MOSTRARERRO
+2:      clickar()
+        numerotxtbx.Focus()
+30:     Exit Sub
+MOSTRARERRO:
+        MsgBox("Sub verifbtn_Click: Erro número #" & Str$(Err.Number) & " na linha " & Str$(Erl) & " - " & Err.Description & " - gerado por " & Err.Source)
+        Resume Next
+    End Sub
+
+
+
+    Public Function letrar(ByVal valor As Integer)
+1:      On Error GoTo MOSTRARERRO
+2:      letrar = Chr(64 + valor)
+        Exit Function
+MOSTRARERRO:
+        MsgBox("function letrar: Erro número #" & Str$(Err.Number) & " na linha " & Str$(Erl) & " - " & Err.Description & " - gerado por " & Err.Source)
+        Resume Next
+    End Function
+
+
+    Public Function getNumeric(value As String) As String
+        Dim output As StringBuilder = New StringBuilder
+        For i = 0 To value.Length - 1
+            If IsNumeric(value(i)) Then
+                output.Append(value(i))
+            End If
+        Next
+        Return output.ToString()
+    End Function
+
+
+    Sub focoaviado() Handles aviadotxtbx.GotFocus
+        If aviadotxtbx.Text = "" And numerotxtbx.Text = "" Then
+            numerotxtbx.Focus()
+        End If
+    End Sub
+
+
+    Private Sub aviadotxtbx_TextChanged(sender As Object, e As EventArgs) Handles aviadotxtbx.TextChanged
+        If Len(aviadotxtbx.Text) = 1 Then
+            caractp1 = aviadotxtbx.Text
+            If caractp1 Like "#" Then
+                clickar()
+                numerotxtbx.Focus()
+            End If
+        End If
+        If aviadotxtbx.Text = "" And numerotxtbx.Text = "" Then
+            numerotxtbx.Focus()
+        End If
+    End Sub
+End Class
